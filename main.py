14742f672b081cf463dbd8f9197fb82607356421
Sammy Mishinev <sam.mishinev@gmail.com>
2025-06-02 14:07:41 +0300

    Replaced raindrop image and scaled it

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = 'center'
 HEIGHT = 1200
 WIDTH = 2000
 
-def draw()
+raindrop = pygame.image.load('images/raindrop.png')
+scaled_raindrop = pygame.transform.scale(raindrop, (100, 100))
+pygame.image.save(scaled_raindrop, 'images/raindrop.png')
+
+raindrop = Actor("raindrop.png")
+
+def draw():
     pass
 
 pgzrun.go()
